a0cec3bc3690ed2641ab25d68bda02745153a891
Amoghdoescoding <81229958+Amoghdoescoding@users.noreply.github.com>
2021-05-26 19:52:04 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+function loading_troll() {
+    document.getElementById("loading_troll").src="trollimg.gif"
+}
+function loading_troll2() {
+    document.getElementById("loading_troll").src="loading.gif"
+}
